ee2fe003809130c3782d46b4803229e4fbe5cdd0
aidanamamasharipova15-cpu <aidanamamasharipova15@gmail.com>
2025-11-29 22:57:00 +0600

    Animal game and guess the number

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,81 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Animal {
+    static int hunger = 30;
+    static int energy = 70;
+    static int mood = 60;
+     static void main(String[] args) {
+        start();
+    }
+    static void start() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> game = new ArrayList<>();
+        System.out.println("--------- Your animal ---------");
+        System.out.println("⊹ ₊  ⁺‧₊˚ ♡ ପ(๑•ᴗ•๑)ଓ ♡˚₊‧⁺ ₊ ⊹");
+        System.out.println("Enter the name of your animal: ");
+        String name = sc.nextLine();
+        game.add(name);
+        System.out.println("Your animal " + name + " is created!");
+        boolean alive = true;
+        while (alive) {
+            System.out.println("----- Status -----");
+            System.out.println("Name: " + name);
+            System.out.println("Hunger: " + hunger);
+            System.out.println("Energy: " + energy);
+            System.out.println("Mood: " + mood);
+            System.out.println("------------------");
+
+            System.out.println("Select an action:");
+            System.out.println("1 — Feed 😊");
+            System.out.println("2 — Play ⚽️");
+            System.out.println("3 — Go to bed 🛌");
+            System.out.println("4 — Not do anything 🐱");
+            System.out.println("5 — Exit 🚪");
+            System.out.print("Your choice: ");
+            String action = sc.nextLine();
+            game.add(action);
+            switch (action) {
+                case "1":
+                    hunger -= 20;
+                    mood += 5;
+                    System.out.println(name + " ate 🍗");
+                    break;
+                case "2":
+                    mood += 20;
+                    hunger += 10;
+                    energy -= 15;
+                    System.out.println("You played with " + name + " ⚽️");
+                    break;
+                case "3":
+                    energy += 25;
+                    hunger += 10;
+                    System.out.println(name + " slept 😴");
+                    break;
+                case "4":
+                    hunger += 5;
+                    mood -= 5;
+                    System.out.println(name + " is doing nothing 🐱");
+                    break;
+
+                case "5":
+                    System.out.println("Exiting the game...");
+                    return;
+                default:
+                    System.out.println("Incorrect input!");
+                    continue;
+            }
+            hunger += 3;
+            mood -= 2;
+            energy -= 1;
+            hunger = Math.max(0, Math.min(100, hunger));
+            mood = Math.max(0, Math.min(100, mood));
+            energy = Math.max(0, Math.min(100, energy));
+            if (hunger >= 100 || energy <= 0 || mood <= 0) {
+                System.out.println(" Your pet " + name + " died...");
+                alive = false;
+            }
+        }
+        System.out.println("Game Over.");
+    }
+}
---
+++ src/main/java/org/example/NumberCuess.java
@@ -0,0 +1,47 @@
+package org.example;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+public class NumberCuess {
+     static void main(String[] args) {
+        Game();
+    }
+    static void Game () {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        ArrayList<String> text = new ArrayList<>();
+        System.out.println("Welcome to the game < Guess the number >!");
+        System.out.println("Select difficulty level:");
+        System.out.println("1 - Easy   (1–50)");
+        System.out.println("2 - Medium (1–100)");
+        System.out.println("3 - Hard   (1–500)");
+        System.out.println("Enter your choice:");
+        String choice = sc.nextLine();
+        text.add(choice);
+        int max = 0;
+        if (choice.equals("1")) {
+            max = 50;
+        } else if (choice.equals("2")) {
+            max = 100;
+        } else if (choice.equals("3")) {
+            max = 500;
+        } else {
+            System.out.println("Wrong choice!");
+            return;
+        }
+        int secret = random.nextInt(max) + 1;
+        System.out.println("I guessed a number from 1 to " + max + ". Try to guess!");
+        while (true) {
+            System.out.print("Your answer: ");
+            int guess = sc.nextInt();
+            if (guess == secret) {
+                System.out.println("Correct! You guessed the number!");
+                break;
+            } else if (guess < secret) {
+                System.out.println("My number is bigger");
+            } else {
+                System.out.println("My number is smaller.");
+            }
+        }
+    }
+}
